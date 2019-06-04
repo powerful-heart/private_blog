ac4e5960eeace1d154babde082d4095a2a09dc8c
jason <LSJason113@163.com>
2019-06-04 20:55:14 +0800

    修复bug

--- bbs/bbs/settings.py
+++ bbs/bbs/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'bbs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'views')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
